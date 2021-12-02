f75fcad21bd5b48166a90a6b68d2f87cac7cb0a1
jyothishkjoy <jyothish.kj@gmail.com>
2021-12-03 07:55:23 +1100

    initial commit

---
+++ src/test/java/com/example/Demo_Application_1/DemoApplication1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Demo_Application_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplication1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
